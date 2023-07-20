3efefdab867f52a44fd9144789dfe1477993679e
student <student@gnu.ac.in>
2023-07-20 08:37:00 +0530

    Practical 1

---
+++ Practical1.8.kt
@@ -0,0 +1,20 @@
+import java.util.Arrays
+
+fun main(){
+
+    val a = arrayOf(5,3,2,1,12,4)
+    println(Arrays.deepToString(a))
+
+    var y = IntArray(3)
+    y[0] = 5;
+    y[1] = 4;
+    y[2] = 1;
+    for (x in y)
+    {
+        println(x)
+
+    }
+
+
+
+    }
