c950608acec0d7463ddd4db65330a6d64bd6f66f
Smit Modi <smitmodi21@gnu.ac.in>
2023-07-26 13:22:09 +0530

    Practical 1

---
+++ Practical1.2.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val I: Int = 10
+    val D: Double = I.toDouble()
+    val St: String = "123"
+    val S: String = "123.45"
+    val In: Int = St.toInt()
+    val Do: Double = S.toDouble()
+
+    println("$I is Integer")
+    println("$D is Double from Integer")
+    println("$St is String")
+    println("$In is Integer from String")
+    println("$S is String")
+    println("$Do is Double from String")
+
+}
